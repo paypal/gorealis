0b43a58b15e52837a5042e9a9a958929bc2133e2
Renan DelValle <renanidelvalle+noreply@gmail.com>
2018-12-12 14:14:31 -0800

    Refactoring test to reduce code size.

--- task_test.go
+++ task_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAuroraTask_Clone_DockerContainer(t *testing.T) {
+func TestAuroraTask_Clone(t *testing.T) {
 
 	task0 := realis.NewTask().
 		Environment("development").
@@ -48,34 +48,10 @@ func TestAuroraTask_Clone_DockerContainer(t *testing.T) {
 	task1 := task0.Clone()
 
 	assert.EqualValues(t, task0, task1, "Clone does not return the correct deep copy of AuroraTask")
-}
 
-func TestAuroraTask_Clone_MesosContainer(t *testing.T) {
-
-	task0 := realis.NewTask().
-		Environment("development").
-		Role("ubuntu").
-		Name("this_is_a_test").
-		ExecutorName(aurora.AURORA_EXECUTOR_NAME).
-		ExecutorData("{fake:payload}").
-		CPU(10).
-		RAM(643).
-		Disk(1000).
-		IsService(true).
-		AddPorts(10).
-		Tier("preferred").
-		MaxFailure(23).
-		AddURIs(true, true, "testURI").
-		AddLabel("Test", "Value").
-		AddNamedPorts("test").
-		AddValueConstraint("test", false, "testing").
-		AddLimitConstraint("test_limit", 1).
-		AddDedicatedConstraint("ubuntu", "name").
-		Container(realis.NewMesosContainer().
-			AppcImage("test", "testing").
-			AddVolume("test", "test", aurora.Mode_RW))
-
-	task1 := task0.Clone()
-
-	assert.EqualValues(t, task0, task1, "Clone does not return the correct deep copy of AuroraTask")
+	task0.Container(realis.NewMesosContainer().
+		AppcImage("test", "testing").
+		AddVolume("test", "test", aurora.Mode_RW))
+	task2 := task0.Clone()
+	assert.EqualValues(t, task0, task2, "Clone does not return the correct deep copy of AuroraTask")
 }
