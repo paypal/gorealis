c90db4773c24243a95ce8be7978d4f4f32ee0ff9
Renan DelValle <renanidelvalle+noreply@gmail.com>
2019-04-29 17:20:25 -0700

    Adding set up and tear down to run tests script and renanming script to runTests since it's applicable to linux and mac.

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+docker-compose up -d
+
+# If running docker-compose up gives any error, don't do anything.
+if [ $? -ne 0 ]; then
+    exit
+fi
+
 # Since we run our docker compose setup in bridge mode to be able to run on MacOS, we have to launch a Docker container within the bridge network in order to avoid any routing issues.
 docker run -t -v $(pwd):/go/src/github.com/paypal/gorealis --network gorealis_aurora_cluster golang:1.10-stretch go test -v github.com/paypal/gorealis $@
+
+docker-compose down
