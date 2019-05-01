75f44dedccfd4471f1c7e8cf764498f7afc8c06a
Renan DelValle <renanidelvalle+noreply@gmail.com>
2019-05-01 09:18:45 -0700

    Added option for container to automatically remove itself after it finishes.

--- runTests.sh
+++ runTests.sh
@@ -8,6 +8,6 @@ if [ $? -ne 0 ]; then
 fi
 
 # Since we run our docker compose setup in bridge mode to be able to run on MacOS, we have to launch a Docker container within the bridge network in order to avoid any routing issues.
-docker run -t -v $(pwd):/go/src/github.com/paypal/gorealis --network gorealis_aurora_cluster golang:1.10-stretch go test -v github.com/paypal/gorealis $@
+docker run --rm -t -v $(pwd):/go/src/github.com/paypal/gorealis --network gorealis_aurora_cluster golang:1.10-stretch go test -v github.com/paypal/gorealis $@
 
 docker-compose down
